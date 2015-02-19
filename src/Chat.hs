96a78a186916eb18f70e60ef90f89e833ebacacf
Diego Pontoriero <dpontor@gmail.com>
2015-02-18 23:23:49 -0800

    Accept some clients!

--- src/Chat.hs
+++ src/Chat.hs
@@ -1,5 +1,7 @@
 -- | CS240h Lab 2 Chat Server
 module Chat (chat) where
+
+import Control.Concurrent
 import Control.Exception
 import Network
 import System.Environment
@@ -9,18 +11,27 @@ getPort :: IO PortID
 getPort = do
   portStr <- lookupEnv "CHAT_SERVER_PORT"
   let port = case portStr of
-      Just str -> fromIntegral (read str :: Int)
-      Nothing  -> 1617
+        Just str -> fromIntegral (read str :: Int)
+        Nothing  -> 1617
   return (PortNumber port)
+
+talk :: Handle -> IO ()
+talk h = do
+  hPutStrLn h "Hi there, what's your name?"
+  talk h
+  -- name <- hGetLine h
+  -- hPutStrLn h $ "What's up, " ++ name
+  -- hClose h
+
+loop :: Socket -> IO ()
+loop s = do
+  (h, _, _) <- accept s
+  forkIO $ finally (talk h) (hClose h)
+  loop s
 
 -- | Chat server entry point.
 chat :: IO ()
 chat = do
   portNumber <- getPort
   bracket (listenOn portNumber) sClose $ \s -> do
-    bracket (accept s) (\(h, _, _) -> hClose h) $
-      \(h, host, port) -> do
-        putStrLn $ "Connection from host " ++ host
-                  ++ " port " ++ show port
-        hPutStrLn h "Hi there."
-
+    loop s
